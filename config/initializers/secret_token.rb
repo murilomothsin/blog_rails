0c16a33a9eb716dd900bb3ef6f98e86a5a8187c1
Murilo Mothsin <mothsin@hotmail.com>
2013-06-12 00:48:06 -0300

    scaffold of Posts

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = 'b294d001d4655cf8519a6b9263d2d2dd9863aac84fc362a9d183a7a850a72733131d5abde7331c05c7a0f062480db034097c0355adf1048768e8d32b11b4ce39'
